ab828ae2d3f334e32fbb439612e039d752035c12
Bryon Smith <webmastersmith37@gmail.com>
2022-08-22 14:14:13 -0500

    'minor commit'

--- practice.py
+++ practice.py
@@ -12,37 +12,59 @@
 # print(s.upper() if upper_num > lower_num else s.lower())
 
 # find num vowels
-# sampleInput = input() 
-# # result = -404; 
+# sampleInput = input()
+# # result = -404;
 # vowels = ["a", "e", "i", "o", "u"]
 # num_vowels = 0
 # for i in sampleInput:
 #     if i in vowels:
 #         num_vowels += 1
 
-# #write your Logic here:  
+# #write your Logic here:
 
 
-# #OUTPUT [uncomment & modify if required]  
-# print(num_vowels) 
+# #OUTPUT [uncomment & modify if required]
+# print(num_vowels)
 
 
-sampleInput = "The big dog jumped high over the lazy red fox and the man with the red hat sat high in the chair and laughed"
-# result = -404; 
-s = list(map(lambda x: x.lower(), sampleInput.split(' ')))
-#write your Logic here:  
+# sampleInput = "The big dog jumped high over the lazy red fox and the man with the red hat sat high in the chair and laughed"
+# # result = -404;
+# s = list(map(lambda x: x.lower(), sampleInput.split(' ')))
+# #write your Logic here:
+# print(s)
+# unique = set(s)
+# d = {}
+# for i in unique:
+#     d[i] = 0
+
+# for i in s:
+#     d[i] += 1
+
+# over_one = []
+# for k,v in d.items():
+#     if v > 1:
+#         over_one.append(f'{k}: {v}')
+# # print(l)
+# print("\n".join(sorted(over_one)))
+
+
+# coding challenge
+# 'ahbuei' is legigraphically lowest
+# s = list('auehib')  # 'ahbuei' is legigraphically lowest
+
+# convert unicode to list of ints,
+s = list([ord(x) - 96 for x in 'auehib'])
+
+# list of vowels converted to int.
+# aeiou
+v = [1, 5, 9, 15, 21]
+
+result = []
+# check if previous value is vowel, then see if moving letter will
+for idx, x in enumerate(s):
+    # check if char can be moved. Is bigger than list.
+    # if x < s[idx]:
+    # check if char is vowel
+
+    # print(str(i).encode('utf-8').decode('utf-8'))
 print(s)
-unique = set(s)
-d = {}
-for i in unique:
-    d[i] = 0
-
-for i in s:
-    d[i] += 1
-
-over_one = []
-for k,v in d.items():
-    if v > 1:
-        over_one.append(f'{k}: {v}')
-# print(l)
-print("\n".join(sorted(over_one)))
